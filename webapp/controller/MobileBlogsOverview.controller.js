c4a63f8f11a5f45360296a4dceab26ccc52689d5
iharpanasiuk <ihar.panasiuk@leverx.com>
2019-05-23 10:39:04 +0000

    added new start page added mobile blogs overview page added carousel to mobile page

--- webapp/controller/MobileBlogsOverview.controller.js
+++ webapp/controller/MobileBlogsOverview.controller.js
@@ -3,32 +3,21 @@ sap.ui.define([
 ], function (Controller) {
 	"use strict";
 
-	return Controller.extend("tc.tagCloud.controller.StartView", {
+	return Controller.extend("tc.tagCloud.controller.MobileBlogsOverview", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf tc.tagCloud.view.StartView
+		 * @memberOf tc.tagCloud.view.MobileBlogsOverview
 		 */
 		onInit: function () {
 
-			var oRouter = sap.ui.core.UIComponent.getRouterFor(this.getView());
-
-			if (sap.ui.Device.system.phone) {
-
-				oRouter.navTo("MobileBlogsOverview");
-
-			} else {
-
-				oRouter.navTo("RouteTag");
-
-			}
-		}
+		},
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf tc.tagCloud.view.StartView
+		 * @memberOf tc.tagCloud.view.MobileBlogsOverview
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -37,7 +26,7 @@ sap.ui.define([
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf tc.tagCloud.view.StartView
+		 * @memberOf tc.tagCloud.view.MobileBlogsOverview
 		 */
 		//	onAfterRendering: function() {
 		//
@@ -45,7 +34,7 @@ sap.ui.define([
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf tc.tagCloud.view.StartView
+		 * @memberOf tc.tagCloud.view.MobileBlogsOverview
 		 */
 		//	onExit: function() {
 		//
